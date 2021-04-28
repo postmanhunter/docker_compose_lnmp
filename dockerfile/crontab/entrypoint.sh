4240345482072b1c6228a7a41aab48aa76efa816
hunter <1039526501@qq.com>
2021-04-28 19:30:33 +0800

    修改crontab的执行人

--- dockerfile/crontab/entrypoint.sh
+++ dockerfile/crontab/entrypoint.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-crontab -u www-data /var/www/html/laravel/scripts/crontab.ini
+crontab /var/www/html/laravel/scripts/crontab.ini
 touch /var/log/cron
 /etc/init.d/cron start
 tail -f /var/log/cron
