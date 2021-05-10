01f92b37623f02d3326cf3cb0b9227edb2f576a3
hunter <1039526501@qq.com>
2021-05-10 20:41:47 +0800

    修改logstash配置文件地址

--- bin/begin.sh
+++ bin/begin.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-mkdir -p /data/{www,logs/{nginx,php72,php73,php74,php80,redis,mysql57,mysql80},elasticsearch/{data,plugins},logstash,elk,redis,mysql57,mysqlback57,mysql80,mysqlback80}
+mkdir -p /data/{www,logs/{nginx,php72,php73,php74,php80,redis,mysql57,mysql80,logstash/{logs}},elasticsearch/{data,plugins},logstash,elk,redis,mysql57,mysqlback57,mysql80,mysqlback80}
 
 mv ./docker /data/
 cd /data/docker
